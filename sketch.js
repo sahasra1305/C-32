fefb3d241c77739b74a33d89dd859c2401192530
sahasra1305 <anjanasahasra@gmail.com>
2021-10-10 14:06:06 +0530

    changed the background

--- sketch.js
+++ sketch.js
@@ -10,8 +10,13 @@ var bird, slingshot;
 
 var gameState = "onSling";
 
+var bg;
+
+var score;
+
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+  getBackgroundImg();
+
 }
 
 function setup(){
@@ -45,18 +50,26 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg)
+        background(backgroundImg);
+        noStroke()
+        textSize(35)
+        fill("white")
+        text("Score "+score,width-300,50)
+
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +98,19 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+async function getBackgroundImg(){
+    var response=await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON=await response.json()
+    var datetime=responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+
+    if(hour>=06 && hour<=19){
+        bg="sprites/bg.png";
+    }
+    else{
+        bg="sprite/bg2.png";
+    }
+    backgroundImg=loadImage(bg);
+    console.log(backgroundImg)
 }
